fb04e762500d9f560be2db885733ca92cedd17fb
Altenburg, Swaibu <SwaibuAltenburg@gmail.com>
2018-09-25 15:51:33 +0200

    Init check in

---
+++ TestForGit/src/Main.java
@@ -0,0 +1,16 @@
+
+public class Main {
+	
+	public static void main(String [] args) {
+		
+		Test test = new Test();
+		displayGreeting();
+		test.action();
+		
+		
+	}
+	private static void displayGreeting() {
+		System.out.println("Hello World!!!");
+	}
+
+}
---
+++ TestForGit/src/Test.java
@@ -0,0 +1,13 @@
+
+public class Test {
+	
+	void action () {
+		
+		System.out.println("do action!!!");
+	}
+	
+	public Test() {
+		
+	}
+
+}
